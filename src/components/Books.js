4c54602027d71ff6f1135ba680d59ca2e7244b1f
root <rakesh.sahu694@gmail.com>
2020-05-26 06:39:10 +0900

    update for windows

--- src/components/Books.js
+++ src/components/Books.js
@@ -8,91 +8,50 @@ import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import '../styling/books.css'
 
-const useStyles = makeStyles({
-  root: {
-    display: "flex",
-    width: "20%",
-    backgroundColor: "grey",
-    flexDirection: "column",
-    height: "100%",
-    borderRadius : 6
-  },
-  content : {
-    display : 'flex',
-    flex:1,
-    flexDirection :'column',
-    alignItems : 'flex-start',
-    padding : '2%'
-  },
-  image: {
-    display: "flex",
-    flex: 1,
-    justifyContent: "center",
-    width: "100%",
-    height: 150,
-    padding : 60
-  },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)"
-  },
-  title: {
-    fontSize: 14,
-    width: 34
-  },
-  pos: {
-    padding : 6,
-    fontSize : 13,
-    width : '100%'
-
-  }
-});
 
 export default function Books() {
-  const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
+
 
   return (
     <div className = "bookContainer">
-    <Card className={classes.root}>
+    <Card className= "mainCard" style = {{ backgroundColor : 'pink'}}  >
       <div >
-        < div>
-          <Card className={classes.image} >
-            <Typography>
+        
+          <Card >
+          < div className = "image" >
+            <Typography >
               Hi
             </Typography>
+            </div>
           </Card>
+        
+        <div className = "content">
+          
+          <div className = "bookTitle" > {'Anthony hoopkins'} </div>
+       
+        <div className = "textStyle">
+        
+         {'adjective'} 
+      
         </div>
-        <div className = {classes.content}>
-          <div>
-        <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        </div>
-        <div>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        </div>
-        <div>
-        <Typography className={classes.pos} color="textSecondary">
-          Rs 445
-        </Typography>
+        <div className = "textStyle">
+        
+        {'Rs 445'}
+        
         </div>
         </div>
       </div>
       <div className = "cardbutton">
         <Card raised={true} >
-          <Typography className={classes.pos} color="textSecondary">
-            Add to Cart
-          </Typography>
+          <div className = "pos" >
+            {'Add to Cart'}
+          </div>
         </Card>
 
         <Card  raised={true}>
-          <Typography className={classes.pos} color="textSecondary">
-            Wishlist
-          </Typography>
+          <div className = "pos" >
+            {'Wishlist'}
+          </div>
         </Card>
       </div>
     </Card>
